3b389402f9ba29855c98c784ad5963f87ab42250
HIS <hisjosuapro@gmail.com>
2023-02-02 13:51:22 +0100

    Portfolio V2.1

--- Header - Footer/header.php
+++ Header - Footer/header.php
@@ -46,3 +46,48 @@ body:before {
     }
     </style>
 </head>
+<body>
+  <div class="container"></div>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="../index.php">Portfolio</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+            <div class="collapse navbar-collapse" id="navbarColor01">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="../index.php">Accueil
+                    <span class="sr-only">(current)</span>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="../presentation.html">Présentation</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Projets / Réalisations</a>
+                  <div class="dropdown-menu">
+                    <a class="dropdown-item" href="../Sous-page/ProjetA.html">Projet A</a>
+                    <a class="dropdown-item" href="../Sous-page/ProjetB.html">Projet B</a>
+                    <div class="dropdown-divider"></div>
+                  </div>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Expérience professionnel</a>
+                  <div class="dropdown-menu">
+                    <a class="dropdown-item" href="../Sous-page/StageN°1.html">Stage entreprise A</a>
+                    <a class="dropdown-item" href="../Sous-page/StageN°2.html">Stage entreprise B</a>
+                    <div class="dropdown-divider"></div>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="../veille-technologique.html">Veille technologique</a>
+                </li>
+                  <li class="nav-item">
+                  <a class="nav-link" href="../contact.html">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+    </header>
